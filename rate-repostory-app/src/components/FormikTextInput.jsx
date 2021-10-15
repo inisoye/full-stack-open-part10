20359c461ffdd1d277771604c9bf1352688ad254
Inioluwa Akinyosoye <inisoye@gmail.com>
2021-10-15 09:39:51 +0100

    Add exercise 10.8

---
+++ rate-repostory-app/src/components/FormikTextInput.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { useField } from 'formik';
+
+import TextInput from './TextInput';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  textInput: {
+    padding: 15,
+    marginBottom: 15,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#D3D3D3',
+    borderRadius: 4,
+  },
+  errorText: {
+    marginTop: 5,
+  },
+});
+
+const FormikTextInput = ({ name, ...props }) => {
+  const [field, meta, helpers] = useField(name);
+  const showError = meta.touched && meta.error;
+
+  return (
+    <>
+      <TextInput
+        onChangeText={(value) => helpers.setValue(value)}
+        onBlur={() => helpers.setTouched(true)}
+        value={field.value}
+        error={showError}
+        style={styles.textInput}
+        {...props}
+      />
+      {showError && <Text style={styles.errorText}>{meta.error}</Text>}
+    </>
+  );
+};
+
+export default FormikTextInput;
--- rate-repostory-app/src/components/SignIn.jsx
+++ rate-repostory-app/src/components/SignIn.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { StyleSheet, Pressable, View } from 'react-native';
+import { Formik } from 'formik';
 
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    backgroundColor: 'white',
+  },
+  submitButton: {
+    padding: 15,
+    backgroundColor: '#0366d6',
+    borderRadius: 4,
+  },
+  submitButtonText: {
+    textAlign: 'center',
+    color: 'white',
+  },
+});
+
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => (
+        <View style={styles.container}>
+          <FormikTextInput name="username" placeholder="Username" />
+
+          <FormikTextInput
+            name="password"
+            placeholder="Password)"
+            secureTextEntry
+          />
+
+          <Pressable style={styles.submitButton} onPress={handleSubmit}>
+            <Text fontWeight="bold" style={styles.submitButtonText}>
+              Sign in
+            </Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
+  );
 };
 
 export default SignIn;
