92c9cfbee2ab3b10b733ac2f839533bee2137964
Inioluwa Akinyosoye <inisoye@gmail.com>
2021-10-16 20:10:23 +0100

    Add exercise 10.15

--- rate-repostory-app/src/hooks/useSignIn.js
+++ rate-repostory-app/src/hooks/useSignIn.js
@@ -1,13 +1,20 @@
-import { useMutation } from '@apollo/client';
+import { useMutation, useApolloClient } from '@apollo/client';
+
 import { SIGN_IN } from '../graphql/mutations';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(SIGN_IN);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
 
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({
       variables: { credentials: { username, password } },
     });
+
+    await authStorage.setAccessToken(data.authorize.accessToken);
+    apolloClient.resetStore();
 
     return data;
   };
